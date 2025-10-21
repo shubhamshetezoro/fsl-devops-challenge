728bff7b39022f8d60cb33d8355e466360be6144
shubhamshetezoro <shubham.shete@zoro.co.uk>
2025-10-21 23:54:46 +0530

    Demo: Clean code change for successful CI test

--- codebase/rdicidr-0.1.0/src/App.js
+++ codebase/rdicidr-0.1.0/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>CIDR Calculator - DevOps Challenge</div>
+        <div>CIDR Calculator - CI Success Demo</div>
       </header>
       <IPv4Addr />
       <footer>
